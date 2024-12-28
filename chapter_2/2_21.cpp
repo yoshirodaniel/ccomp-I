7f336c61bf703b4c7fd0a9e85bd2a7e3c99d30ac
yoshirodaniel <yoshirodaniel1408@gmail.com>
2024-12-28 11:14:29 -0500

    2_21 y 2_22

---
+++ chapter_2/2_21.cpp
@@ -0,0 +1,29 @@
+/*(Displaying Large Letters) Write a program that prints C++ as follows:
+  CCC   +     +
+ C      +     +
+C     +++++ +++++
+ C      +     +
+  CCC   +     +
+  */
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+   system ("CLS");
+
+   //char espacio='C';
+   cout<<"  CCC   +      +"<<endl;
+   cout<<" C      +      +"<<endl;
+   cout<<"C     +++++  +++++"<<endl; 
+   cout<<" C      +      +"<<endl;
+   cout<<"  CCC   +      +"<<endl;
+
+   cout << "*****\n****\n***\n **\n*\n " << endl;
+
+   return 0;
+}
+
