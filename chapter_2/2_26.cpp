c7b3fafa24683331a0bb2d304dd7dc432b7f5e39
yoshirodaniel <yoshirodaniel1408@gmail.com>
2024-12-30 22:49:04 -0500

    2_26

---
+++ chapter_2/2_26.cpp
@@ -0,0 +1,22 @@
+/*(Checkerboard Pattern) Display the following checkerboard pattern with eight output
+statements, then display the same pattern using as few statements as possible.*/
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main () {
+   system("CLS");
+   cout<<"* * * * * * * *"<<endl;
+   cout<<" * * * * * * * *"<<endl;
+   cout<<"* * * * * * * *"<<endl;
+   cout<<" * * * * * * * *"<<endl;
+   cout<<"* * * * * * * *"<<endl;
+   cout<<" * * * * * * * *"<<endl;
+   cout<<"* * * * * * * *"<<endl;
+   cout<<" * * * * * * * *"<<endl;
+   cout<<"* * * * * * * *"<<endl;
+   cout<<" * * * * * * * *"<<endl;
+
+}
